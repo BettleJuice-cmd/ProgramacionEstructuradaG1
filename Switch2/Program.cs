31db7ba2b104b1dfbe9d3e62702084646aa65312
BettleJuice <asdiazm@uamv.edu.ni>
2025-08-29 07:43:16 -0600

    Subiendo ejercicio Switch2

---
+++ Switch2/Program.cs
@@ -0,0 +1,26 @@
+﻿int carreras;
+string nombCarreras;
+
+
+Console.Write("Ingrese un numero: ");
+carreras = int.Parse(Console.ReadLine());
+
+switch (carreras)
+
+{
+    case 1:
+        nombCarreras = "Medicina";
+        break;
+    case 2:
+        nombCarreras = "Arquitectura";
+        break;
+    case 3:
+        nombCarreras = "Sistemas";
+        break;
+    default:
+        nombCarreras = "Numero invalido";
+        break;
+}
+
+Console.WriteLine(nombCarreras);
+
